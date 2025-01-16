fd6f15681ba30d9f85fe26368c456f37a3935e2a
nakao yoichi <yoichi@ad.physicist00.jp>
2025-01-16 22:40:58 +0900

    文字化け修正

--- mysql/docker/resources/mysql/initdb.d/init.sql
+++ mysql/docker/resources/mysql/initdb.d/init.sql
@@ -1,9 +1,11 @@
+SET NAMES utf8mb4;
+
 create table sample1(
     id int auto_increment,
     col1 text,
     col2 text,
     primary key (id)
-) ;
+);
 
 insert into sample1(col1, col2)values('aaa', '1111');
 insert into sample1(col1, col2)values('bbb', '2222');
